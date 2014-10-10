b26a147fb4f63e4ab9be691c7f020b2781602805
OBrienLT <liam.t.obrien@hotmail.com>
2014-10-09 21:56:38 -0400

    Added 5 seed wines.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Wine.create(name:"Cabernet Napa", year:2014, winery:"Dominican Oaks", country:"United States", varietal:"Cabernet Sauvignon")
+Wine.create(name:"Moulis en Medoc", year:2010, winery:"Chateau Duplessis", country:"France", varietal:"Bordeaux Blend")
+Wine.create(name:"Marlborough Sauvignon Blanc", year:2012, winery:"Framingham", country:"New Zealand", varietal:"Sauvignon Blanc")
+Wine.create(name:"Domaine Loubejac", year: 2010, winery:"Willamette Valley", country:"United States", varietal:"Pinot Noir")
+Wine.create(name:"Ropiteau Chardonnay", year:2014, winery:"Ropiteau", country:"France", varietal:"Chardonnay")
