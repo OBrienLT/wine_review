4ce9a7d9f5907c8943630f51b2e776cd71fdeed6
OBrienLT <liam.t.obrien@hotmail.com>
2014-10-16 00:01:52 -0400

    Added validations to Wine class.

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,2 +1,12 @@
 class Wine < ActiveRecord::Base
+
+	VARIETALS = [ "Barbera", "Cabernet Sauvignon", "Chardonnay", "Grenache", "Malbec", "Muscat", "Pinot Grigio", 
+		"Pinot Noir", "Riesling", "Sauvignon Blanc", "Zinfandel" ]
+
+	validates :name, :year, :country, presence: true
+
+	validates :year, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+	validates :varietal, inclusion: { in: VARIETALS }
+
 end
